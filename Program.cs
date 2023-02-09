d424b91ff90e4c8818742e8b7d646de7e5297775
MariaSanchez20-1094 <124853235+MariaSanchez20-1094@users.noreply.github.com>
2023-02-08 23:10:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Calcular el Factorial de un número \n");
+            do
+            {
+                Console.Write("Digite un número : ");
+                num = int.Parse(Console.ReadLine());
+                verificar(num);
+            } while (num < 0);
+            factorial(num);
+            Console.ReadKey();
+        }
+        static void verificar(int n)
+        {
+            if (n < 0)
+            {
+                Console.Write("\n No existe el factorial de un número\n");
+            }
+        }
+        static void factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+            Console.WriteLine("\nEl Factorial de " + n + " es: " + fact);
+        }
+    }
+}
